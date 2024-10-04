6441c0fd369eedb4ebcfebeee03b49d79a886506
Owen Ganza <o.ganza@alu.com>
2024-10-04 22:42:10 +0200

    commit all

--- reminder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ./config/config.env
-source ./modules/functions.sh
-submissions_file="./assets/submissions.txt"
-echo "Assignment: $ASSIGNMENT"
-echo "Days remaining to submit: $DAYS_REMAINING days"
-echo "--------------------------------------------"
-check_submissions $submissions_file
-
--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -f config/config.env ]; then
-  source config/config.env
-fi
-source functions.sh
-source reminder.sh
