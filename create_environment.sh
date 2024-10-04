c23c63cf068ed15350aa16c8fe937fb8b67644a7
Owen Ganza <o.ganza@alu.com>
2024-10-04 21:44:13 +0200

    commit all

---
+++ create_environment.sh
@@ -0,0 +1,6 @@
+mkdir submission_reminder_app/{app,modules,assets,config}
+touch submission_reminder_app/app/reminder.sh
+touch submission_reminder_app/modules/functions.sh
+touch submission_reminder_app/assets/submissions.txt
+touch submission_reminder_app/config/config.env
+touch submission_reminder_app/startup.sh
